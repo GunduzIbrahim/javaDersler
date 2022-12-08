768fca4a6a1d1f624a723220b5fd052283298ca8
ihsan_elif <ihsan_elif@10.107.225.217>
2022-12-08 15:45:29 +0100

    Question1 commit

---
+++ javaDersler/src/assigment/Question1_2.java
@@ -0,0 +1,16 @@
+package assigment;
+
+public class Question1_2 {
+			
+	public static void main (String[] args) {
+			
+			byte age = 25;
+			short classNumber = 365;
+			float height = 1.80f;
+			
+			System.out.println("Your age is : " + age);
+			System.out.println("Your Class number is : " + classNumber);
+			System.out.println("Your heigt is : " + height + " cm\n");
+			
+		}
+}
